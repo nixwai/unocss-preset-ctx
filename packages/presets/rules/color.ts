5228e282b16e42b0f6c4b02c709cf4c96c118cef
nixiaoyu <1149062971@qq.com>
2024-12-21 18:14:32 +0800

    feat(preset): add more context control for css style colors

--- packages/presets/rules/color.ts
+++ packages/presets/rules/color.ts
@@ -1,4 +1,5 @@
 import type { Theme } from '@unocss/preset-mini';
+import type { CSSObject } from 'unocss';
 import type { CustomRule } from '../types';
 import { parseColor } from '@unocss/preset-mini';
 import { mc } from 'magic-color';
@@ -22,8 +23,68 @@ export const color: CustomRule[] = [
   ],
   // Context variable for color
   [
-    /^(text-color|color|c)-ctx-(.+)$/,
-    ([,,s]) => getColorValue(s, 'color', 'text'),
+    /^(?:text-color|text|color|c)-ctx-(.+)$/,
+    cxtColorResolver('color', 'text'),
+  ],
+  [
+    /^outline-color-ctx-(.+)$/,
+    cxtColorResolver('outline-color', 'outline-color'),
+  ],
+  [
+    /^bg-ctx-(.+)$/,
+    cxtColorResolver('background-color', 'bg'),
+  ],
+  [
+    /^(?:underline|decoration)-ctx-(.+)$/,
+    cxtColorResolver('text-decoration-color', 'line'),
+  ],
+  [
+    /^ring-offset-ctx-(.+)$/,
+    cxtColorResolver('--un-ring-offset-color', 'ring-offset'),
+  ],
+  [
+    /^ring-ctx-(.+)$/,
+    cxtColorResolver('--un-ring-color', 'ring'),
+  ],
+  [
+    /^shadow-ctx-(.+)$/,
+    cxtColorResolver('--un-shadow-color', 'shadow'),
+  ],
+  [
+    /^fill-ctx-(.+)$/,
+    cxtColorResolver('fill', 'fill'),
+  ],
+  [
+    /^stroke-ctx-(.+)$/,
+    cxtColorResolver('stroke', 'stroke'),
+  ],
+  [
+    /^text-stroke-ctx-(.+)$/,
+    cxtColorResolver('-webkit-text-stroke-color', 'text-stroke'),
+  ],
+  [
+    /^text-shadow-color-ctx-(.+)$/,
+    cxtColorResolver('--un-text-shadow-color', 'text-shadow'),
+  ],
+  [
+    /^accent-ctx-(.+)$/,
+    cxtColorResolver('accent-color', 'accent'),
+  ],
+  [
+    /^caret-ctx-(.+)$/,
+    cxtColorResolver('caret-color', 'caret'),
+  ],
+  [
+    /^divide-ctx-(.+)$/,
+    cxtColorResolver('border-color', 'divide'),
+  ],
+  [
+    /^(?:filter-)?drop-shadow-color-ctx-(.+)$/,
+    cxtColorResolver('--un-drop-shadow-color', 'drop-shadow'),
+  ],
+  [
+    /^placeholder-ctx-(.+)$/,
+    cxtColorResolver('color', 'placeholder'),
   ],
 ];
 
@@ -50,6 +111,7 @@ export function resolveContextColor(
   theme: Theme,
 ) {
   const parsedColor = parseColor(str, theme);
+
   if (!parsedColor || !parsedColor.color || !parsedColor.cssColor) {
     return '';
   }
@@ -86,14 +148,13 @@ export function resolveContextColor(
 
 /**
  * Use css variables to control color values
- * @param str color name and lightness
  * @param property css property, color | background-color | border-color | ...
  * @param varName Base name for the opacity variable.
  * @return css variables
  *
  * @example
  * ```ts
- * getColorValue('com-400', 'color', 'text')
+ * cxtColorResolver('com-400', 'color', 'text')
  * => {
  *   '--un-text-opacity': 1,
  *   '--ctx-bool-com': 1,
@@ -101,26 +162,28 @@ export function resolveContextColor(
  * }
  * ```
  */
-function getColorValue(str: string, property: string, varName: string) {
-  const [color, alpha] = str.split(/[:/]/);
-  const name = color.replace(/(.*)-\d+/, '$1');
+function cxtColorResolver(property: string, varName: string) {
+  return ([, str]: string[]): CSSObject | undefined => {
+    const [color, alpha] = str.split(/[:/]/);
+    const name = color.replace(/(.*)-\d+/, '$1');
 
-  const colorDefault = ctxName('color', name);
-  const colorH = `var(${ctxName('color', name, 'h')})`;
-  const colorS = `var(${ctxName('color', name, 's')})`;
-  // Dynamic lightness
-  let colorL = `var(${ctxName('color', name, 'l')})`;
-  const lightness = color.match(/.*-(\d+)/)?.[1] || '500';
-  if (lightness) {
-    const diffL = (500 - Number(lightness)) / 10;
-    if (diffL) {
-      const reverse = `var(${ctxName('reverse', name)}, var(${ctxName('reverse')}, 1))`;
-      colorL = `calc(${colorL} + ${reverse} * ${diffL})`;
+    const colorDefault = ctxName('color', name);
+    const colorH = `var(${ctxName('color', name, 'h')})`;
+    const colorS = `var(${ctxName('color', name, 's')})`;
+    // Dynamic lightness
+    let colorL = `var(${ctxName('color', name, 'l')})`;
+    const lightness = color.match(/.*-(\d+)/)?.[1] || '500';
+    if (lightness) {
+      const diffL = (500 - Number(lightness)) / 10;
+      if (diffL) {
+        const reverse = `var(${ctxName('reverse', name)}, var(${ctxName('reverse')}, 1))`;
+        colorL = `calc(${colorL} + ${reverse} * ${diffL})`;
+      }
     }
-  }
 
-  const value: Record<string, string | number> = {};
-  value[`--un-${varName}-opacity`] = alpha ? Number.parseInt(alpha) / 100 : 1;
-  value[property] = `var(${colorDefault}, hsl(${colorH} ${colorS} ${colorL} / var(--un-${varName}-opacity)))`;
-  return value;
+    const value: Record<string, string | number> = {};
+    value[`--un-${varName}-opacity`] = alpha ? Number.parseInt(alpha) / 100 : 1;
+    value[property] = `var(${colorDefault}, hsl(${colorH} ${colorS} ${colorL} / var(--un-${varName}-opacity)))`;
+    return value;
+  };
 }
--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="ctx-color-com_primary ctx-reverse-com-y">
-    <div class="c-ctx-com-600 bg-ctx-com-300">
+  <div class="ctx-color-text_primary ctx-color-bg_gray ctx-reverse-text-y">
+    <div class="c-ctx-text-600 bg-ctx-bg-400">
       Hello
     </div>
-    <div class="c-ctx-com-400 bg-ctx-com-700">
+    <div class="c-ctx-text-400 bg-ctx-bg-600">
       World
     </div>
   </div>
