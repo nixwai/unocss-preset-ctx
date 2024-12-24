46244eb91c22a96047b0d3080f93f8cf196ef9c5
nixiaoyu <1149062971@qq.com>
2024-12-24 10:15:45 +0800

    fix(preset): modify the css context variable name

--- packages/presets/rules/reverse.ts
+++ packages/presets/rules/reverse.ts
@@ -3,15 +3,15 @@ import { ctxName } from '../utils';
 
 export const reverse: CustomRule[] = [
   [
-    /^ctx-reverse-(.+)$/,
+    /^ctx-r-(.+)$/,
     ([, s]) => {
       const name = s.match(/(.*)-[yn]$/)?.[1];
       const value = s.match(/.*([yn])$/)?.[1];
       if (name && value) {
-        return { [ctxName('reverse', name)]: value === 'y' ? -1 : 1 };
+        return { [ctxName('r', name)]: value === 'y' ? -1 : 1 };
       }
       if (value) {
-        return { [ctxName('reverse')]: value === 'y' ? -1 : 1 };
+        return { [ctxName('r')]: value === 'y' ? -1 : 1 };
       }
     },
   ],
--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="ctx-color-text_primary ctx-color-bg_gray ctx-reverse-text-y">
-    <div class="c-ctx-text-600 bg-ctx-bg-400">
+  <div class="ctx-c-text_primary ctx-c-bg_gray ctx-r-text-y">
+    <div class="c-ctx-c-text-600 bg-ctx-c-bg-400">
       Hello
     </div>
-    <div class="c-ctx-text-400 bg-ctx-bg-600">
+    <div class="c-ctx-c-text-400 bg-ctx-c-bg-600">
       World
     </div>
   </div>
