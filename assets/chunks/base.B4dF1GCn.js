ec65d57f111d9060446341b31fce5858fea56792
nixiaoyu <1149062971@qq.com>
2024-12-25 18:57:41 +0800

    update docs

---
+++ assets/chunks/base.B4dF1GCn.js
@@ -0,0 +1,11 @@
+import{_ as n,o as s,c as o,j as c}from"./framework.danP1z9m.js";const r=`<template>\r
+  <div class="ctxs-btn_mx-1,px-2,b-1,b-solid,rounded-md">\r
+    <button class="c-blue ctxs-btn">\r
+      button\r
+    </button>\r
+    <button class="ctxs-btn c-yellow">\r
+      button\r
+    </button>\r
+  </div>\r
+</template>\r
+`,e={},b={class:"ctxs-btn_mx-1,px-2,b-1,b-solid,rounded-md"};function u(a,t){return s(),o("div",b,t[0]||(t[0]=[c("button",{class:"c-blue ctxs-btn"}," button ",-1),c("button",{class:"ctxs-btn c-yellow"}," button ",-1)]))}const l=n(e,[["render",u]]);export{r as T,l as a};
