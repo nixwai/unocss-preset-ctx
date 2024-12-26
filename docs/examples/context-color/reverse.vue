4a4a997a3195dfb383e76257f2a6bc8845e91eac
nixiaoyu <1149062971@qq.com>
2024-12-26 11:59:45 +0800

    fix(docs): optimize docs example

--- docs/examples/context-color/reverse.vue
+++ docs/examples/context-color/reverse.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex ctx-c-bg_gray ctx-c-txt_primary">
-    <div class="px-2 c-ctx-c-txt-700 bg-ctx-c-bg-300">
+  <div class="flex ctx-c-bg_gray ctx-c-txt_primary ctxs-dom_px-2,c-ctx-c-txt-700,bg-ctx-c-bg-300">
+    <div class="ctxs-dom">
       light
     </div>
-    <div class="c-ctx-c-txt-700 bg-ctx-c-bg-300 px-2 ctx-r-y">
+    <div class="ctxs-dom ctx-r-y">
       dark
     </div>
   </div>
