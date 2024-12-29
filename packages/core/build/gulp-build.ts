9334b2fce3d7bef17563bd017871cc8439c8212b
nixiaoyu <1149062971@qq.com>
2024-12-29 12:26:22 +0800

    featL create release and publish script

--- packages/core/build/gulp-build.ts
+++ packages/core/build/gulp-build.ts
@@ -1,8 +1,8 @@
-import { parallel, series } from 'gulp';
+import { series } from 'gulp';
 import { presetOutput } from '../paths';
 import { delPath, run } from '../tasks';
 
 export default series(
   () => delPath(presetOutput),
-  parallel(() => run('vite build', '.')),
+  () => run('vite build'),
 );
