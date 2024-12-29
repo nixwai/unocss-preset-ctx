b2a4e7735af9fe2fccdd10c0a1f85da925bcb58c
nixiaoyu <1149062971@qq.com>
2024-12-29 12:54:14 +0800

    fix: repair run script

--- packages/core/tasks/run.ts
+++ packages/core/tasks/run.ts
@@ -2,13 +2,21 @@ import { spawn } from 'node:child_process';
 
 export async function run(command: string, path = '.') {
   const [cmd, ...args] = command.split(' ');
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const app = spawn(cmd, args, {
       cwd: path, // execution directory for the command
       stdio: 'inherit', // output is shared with the parent process
       shell: true, // shell support is enabled (required for Windows Git-based environments)
     });
     // resolve the Promise when the child process closes.
-    app.on('close', resolve);
+    app.on('close', (code) => {
+      if (code === 0) {
+        resolve(code);
+      }
+      else {
+        reject(new Error(`Command failed with exit code ${code}`));
+      }
+    });
+    app.on('error', reject);
   });
 }
