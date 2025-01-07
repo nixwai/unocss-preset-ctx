69a71d99fd8a605874205f498a6c1505b008dd8c
nixiaoyu <1149062971@qq.com>
2025-01-07 16:34:37 +0800

    feat: create opacity and lightness about content

--- packages/presets/utils/index.ts
+++ packages/presets/utils/index.ts
@@ -9,3 +9,11 @@ export function ctxName(...ary: (string | number)[]) {
 export function hslValue(color?: string) {
   return color?.match(/\d+\s[\d\s]+/)?.[0] || '';
 }
+
+/** var(value, def)) */
+export function toVar(value: string, def?: string | number) {
+  if (def !== undefined) {
+    return `var(${value}, ${def})`;
+  }
+  return `var(${value})`;
+}
