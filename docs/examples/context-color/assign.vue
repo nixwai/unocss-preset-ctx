c5e32b39a9fbfd31e0115725f7d4d0ce09a4a4bd
nixiaoyu <1149062971@qq.com>
2025-01-07 18:25:11 +0800

    feat(docs): add docs about ctx-op and ctx-l

--- docs/examples/context-color/assign.vue
+++ docs/examples/context-color/assign.vue
@@ -1,19 +1,7 @@
 <template>
   <div class="flex ctx-c-test_blue-700 ctx-c-bg_ctx-c-test-300 ctx-c-txt_ctx-c-test/50">
-    <div class="px-2 bg-ctx-c-test c-white">
-      test0
-    </div>
     <div class="px-2 bg-ctx-c-bg-300 c-ctx-c-txt-800">
-      test1
-    </div>
-    <div class="px-2 bg-ctx-c-bg-300 c-ctx-c-txt-800 ctx-r-test-y">
-      test2
-    </div>
-    <div class="px-2 bg-ctx-c-bg-300 c-ctx-c-txt-800 ctx-r-bg-y">
-      test3
-    </div>
-    <div class="px-2 bg-ctx-c-bg-300 c-ctx-c-txt-800 ctx-r-y">
-      test4
+      test
     </div>
   </div>
 </template>
---
+++ docs/examples/context-color/opacity.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex ctx-c-btn_red/80">
+    <button class="bg-ctx-c-btn">
+      button
+    </button>
+    <div class="ctx-op-btn-60">
+      <button class="bg-ctx-c-btn">
+        button
+      </button>
+      <button class="bg-ctx-c-btn/40">
+        button
+      </button>
+      <button class="bg-ctx-c-btn/40 bg-op-20">
+        button
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+button {
+  color: white;
+  padding: 4px 8px;
+  border-radius: 4px;
+  margin: 0 4px;
+}
+</style>
