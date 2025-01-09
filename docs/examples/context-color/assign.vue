9b02ffaf0944f50bbd515c475d136b6b84b11942
nixiaoyu <1149062971@qq.com>
2025-01-09 16:44:55 +0800

    feat(docs): lightness and opacity can be assign by other ctx

--- docs/examples/context-color/assign.vue
+++ docs/examples/context-color/assign.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="flex ctx-c-test_blue-700 ctx-c-bg_ctx-c-test-300 ctx-c-txt_ctx-c-test/50">
-    <div class="px-2 bg-ctx-c-bg-300 c-ctx-c-txt-800">
-      test
+  <div class="flex ctx-c-test_blue-500 ctx-l-test-700 ctx-op-test-50 ctx-c-bg_ctx-c-test ctx-c-txt_ctx-c-test">
+    <div class="px-2 bg-ctx-c-bg c-ctx-c-txt ctx-l-txt_ctx-l-test-800">
+      test1
+    </div>
+    <div class="px-2 bg-ctx-c-bg c-ctx-c-txt ctx-op-txt_ctx-op-test-150">
+      tes2
     </div>
   </div>
 </template>
